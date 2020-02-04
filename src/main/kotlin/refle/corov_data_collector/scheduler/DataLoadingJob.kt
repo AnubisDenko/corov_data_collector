b209ba746c001b72cbe613060fc859972c809a6e
Oliver Refle <orefle@gmail.com>
2020-02-04 12:14:47 +0800

    added local profile for testing and replace quartz with simple spring boot scheduler

--- src/main/kotlin/refle/corov_data_collector/scheduler/DataLoadingJob.kt
+++ src/main/kotlin/refle/corov_data_collector/scheduler/DataLoadingJob.kt
@@ -3,12 +3,15 @@ package refle.corov_data_collector.scheduler
 import org.quartz.Job
 import org.quartz.JobExecutionContext
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import refle.corov_data_collector.service.DataLoader
 
 @Component
-class DataLoadingJob(@Autowired private val dataLoader: DataLoader): Job {
-    override fun execute(context: JobExecutionContext?) {
+class DataLoadingJob(@Autowired private val dataLoader: DataLoader){
+
+    @Scheduled(zone = "Asia/Hong_Kong", cron = "* 8 * * * ?")
+    fun execute() {
         dataLoader.loadData()
     }
 }
