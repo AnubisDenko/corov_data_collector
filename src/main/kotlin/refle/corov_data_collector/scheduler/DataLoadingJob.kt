a9d9c7e242f06993abd515928c22969d667c80cb
Oliver Refle <orefle@gmail.com>
2020-02-13 15:31:01 +0800

    Adding scheduled job execution into coroutine

--- src/main/kotlin/refle/corov_data_collector/scheduler/DataLoadingJob.kt
+++ src/main/kotlin/refle/corov_data_collector/scheduler/DataLoadingJob.kt
@@ -1,5 +1,7 @@
 package refle.corov_data_collector.scheduler
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
@@ -15,12 +17,13 @@ class DataLoadingJob(@Autowired private val dataLoader: DataLoader, @Autowired p
 
     @Scheduled(zone = "Asia/Hong_Kong", cron = "0 0 7-20 * * ?")
     fun execute() {
-        val stopWatch = StopWatch()
-        stopWatch.start()
-        dataLoader.loadData()
-        stopWatch.stop()
+        GlobalScope.launch {
+            val stopWatch = StopWatch()
+            stopWatch.start()
+            dataLoader.loadData()
+            stopWatch.stop()
 
-        scheduledJobRepo.save(ScheduledJobRun("DataLoadingJob", stopWatch.lastTaskTimeMillis, ZonedDateTime.now(ZoneId.of("Asia/Hong_Kong"))))
-
+            scheduledJobRepo.save(ScheduledJobRun("DataLoadingJob", stopWatch.lastTaskTimeMillis, ZonedDateTime.now(ZoneId.of("Asia/Hong_Kong"))))
+        }
     }
 }
