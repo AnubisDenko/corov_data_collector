6306d8e86c32b7fbaa514e6a257a1b9b85e10a73
Oliver Refle <orefle@gmail.com>
2020-03-19 09:13:34 +0800

    made comment field optional

--- src/main/kotlin/refle/corov_data_collector/model/Response.kt
+++ src/main/kotlin/refle/corov_data_collector/model/Response.kt
@@ -3,7 +3,7 @@ package refle.corov_data_collector.model
 
 data class Response(val results:List<Result>, val success:Boolean)
 data class Result(val countryName: String, val provinceName: String, val provinceShortName: String, val confirmedCount: Int,
-                  val suspectedCount: Int, val curedCount: Int, val deadCount: Int, val comment: String,
+                  val suspectedCount: Int, val curedCount: Int, val deadCount: Int, val comment: String? = null,
                   val updateTime: Long, val cities: List<RawCity>?,
                   val continentName: String, val continentEnglishName: String?, val countryEnglishName: String?, val provinceEnglishName: String?)
 data class RawCity(val cityName: String, val confirmedCount: Int, val suspectedCount: Int, val curedCount: Int, val deadCount: Int, val locationId: Int, val cityEnglishName: String?)
